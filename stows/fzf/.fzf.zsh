2729b54e345da92515089f433e861a36bd17a03d
tenmo <tenmo2003@gmail.com>
2025-06-26 17:24:41 +0700

    make .fzf.zsh to work with dynamic home folder (i was too stupid to realize at first :clown:)

--- stows/fzf/.fzf.zsh
+++ stows/fzf/.fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/tenmo/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/tenmo/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 source <(fzf --zsh)
