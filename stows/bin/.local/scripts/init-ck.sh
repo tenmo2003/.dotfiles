1c2a29a289d22557640c94d0418e25a578cdf1c3
tenmo <tenmo2003@gmail.com>
2026-04-08 16:25:38 +0700

    feat: add init-ck.sh

---
+++ stows/bin/.local/scripts/init-ck.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pwd=$(pwd)
+
+cp -r ~/claudekit-engineer/.claude $pwd/
+cp -r ~/claudekit-engineer/docs/ $pwd/
+cp -r ~/claudekit-engineer/plans $pwd/
+cp ~/claudekit-engineer/CLAUDE.md $pwd/
