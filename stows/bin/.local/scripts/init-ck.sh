96d5f387c4fe1001c6c7ca129e3ffdac3bc8d744
tenmo <tenmo2003@gmail.com>
2026-04-10 13:25:06 +0700

    feat: add claude kit to gitignore in initck

--- stows/bin/.local/scripts/init-ck.sh
+++ stows/bin/.local/scripts/init-ck.sh
@@ -6,3 +6,13 @@ cp -r ~/claudekit-engineer/.claude $pwd/
 cp -r ~/claudekit-engineer/docs/ $pwd/
 cp -r ~/claudekit-engineer/plans $pwd/
 cp ~/claudekit-engineer/CLAUDE.md $pwd/
+
+if [[ -f $pwd/.gitignore && -z $(grep -q '.claude' $pwd/.gitignore) ]]; then
+    cat <<EOF >> $pwd/.gitignore
+# ClaudeKit
+.claude/
+docs/
+plans/
+CLAUDE.md
+EOF
+fi
